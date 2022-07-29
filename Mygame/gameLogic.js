4d48f3bcafae43727978adf0dc6ccc4589350694
Kaja <Kaja@SERVER>
2022-07-29 00:07:10 -0700

    rename files, add buttons

--- Mygame/gameLogic.js
+++ Mygame/gameLogic.js
@@ -1,15 +1,18 @@
 ﻿var character = document.getElementById("character");
 var block = document.getElementById("block");
+var losesParagraph = document.getElementById("losesParagraph");
 var loses = 0;
 var isDead = false;
 
 function jump() {
     if (character.classList != "animate") {
         character.classList.add("animate");
+        playAudio();
     }
     setTimeout(function () {
         character.classList.remove("animate");
     }, 500);
+   
 }
 
 var checkDead = setInterval(function () {
@@ -21,8 +24,28 @@ var checkDead = setInterval(function () {
     if (blockLeft > 40)
         if (isDead) {
             loses++;
-            document.getElementById("p1").innerHTML = "Loses : " + loses;
+            console.log(loses);
+            losesParagraph.innerHTML = "Loses : " + loses;
             isDead = false;
         }
 }, 100);
+
+function startGame() {
+    block.classList.add("moveBlock");
+    block.style.animationPlayState = "running";
+}
+
+function stopGame() {
+    block.style.animationPlayState = "paused";
+}
+
+function endGame() {
+    block.classList.remove("moveBlock");
+    losesParagraph.innerHTML = "Loses : 0";
+}
+
+function playAudio() {
+    var audio = new Audio('jumpingSound.wav');
+    audio.play();
+}
 
