83079f0ba9e6fe79b12c7438475814eef2879700
Kaja <Kaja@SERVER>
2022-07-28 22:04:17 -0700

    Initial commit - basic game

---
+++ Mygame/JavaScript.js
@@ -0,0 +1,28 @@
+﻿var character = document.getElementById("character");
+var block = document.getElementById("block");
+var loses = 0;
+var isDead = false;
+
+function jump() {
+    if (character.classList != "animate") {
+        character.classList.add("animate");
+    }
+    setTimeout(function () {
+        character.classList.remove("animate");
+    }, 500);
+}
+
+var checkDead = setInterval(function () {
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if (blockLeft < 40 && blockLeft > 0 && characterTop >= 130) {
+        isDead = true;
+    }
+    if (blockLeft > 40)
+        if (isDead) {
+            loses++;
+            document.getElementById("p1").innerHTML = "Loses : " + loses;
+            isDead = false;
+        }
+}, 100);
+
---
+++ Mygame/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("Mygamer")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Mygamer")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("5f1e7486-e450-49a2-b32b-82eef00f4e3e")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
